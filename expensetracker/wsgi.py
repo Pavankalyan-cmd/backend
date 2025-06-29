dac26b06f93fa9f17002b079b5074bc07e3ce8fa
Pavankalyan Vandanapu <vandanapupavan1@gmail.com>
2025-06-29 23:13:03 +0530

    updated

--- expensetracker/wsgi.py
+++ expensetracker/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+# backend/expensetracker/wsgi.py
+
+
+# This must match your settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'expensetracker.settings')
 
 application = get_wsgi_application()
